682295767dc0af297291ea959f966a113589631e
byf_new <byf_new@126.com>
2019-10-07 23:17:04 +0800

    请假和课程表功能

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-from flask_restful import Api
+# from flask_restful import Api
 #数据库兼容
 import pymysql
 pymysql.install_as_MySQLdb()
 #实例化插件
 csrf = CSRFProtect()
 models = SQLAlchemy()
-api = Api()
+# api = Api()
 
 def create():
     """
@@ -19,7 +19,7 @@ def create():
     app.config.from_object("settings.Config")#加载配置
     models.init_app(app)# models = SQLAlchemy(app) 加载数据库
     #csrf.init_app(app) #加载csrf插件
-    api.init_app(app)
+    # api.init_app(app)
     #导入蓝图
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
